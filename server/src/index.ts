bc2f098918694d13d507520609e2d58c1dbf0f89
vclorenzo <vc.lorenzo16@gmail.com>
2026-05-06 00:23:57 +0800

    updated port on server

--- server/src/index.ts
+++ server/src/index.ts
@@ -50,7 +50,7 @@ app.use("/expenses", expenseRoutes);
 app.use(errorHandler);
 
 // SERVER
-const port = Number(process.env.PORT) || 3001;
+const port = Number(process.env.PORT) || 8000;
 app.listen(port, "0.0.0.0", () => {
   console.log(`server is running on port ${port}`);
 });
